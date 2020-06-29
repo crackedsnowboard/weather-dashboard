40e041b27df541ed290a19b4273ab699a82d291c
Prince of Kochi <mathenjs@gmail.com>
2020-06-28 17:15:24 -0700

    placed listener on city button

---
+++ script.js
@@ -0,0 +1,22 @@
+$(document).ready(function() {
+
+
+
+
+
+
+$("#add-city").on("click", function(event) {
+    event.preventDefault();
+    var cityRequest = $("#weatherQuery").val();
+    console.log(cityRequest);
+    
+
+
+})
+
+
+
+
+
+
+});
